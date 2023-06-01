d406d07057dee374dac49efabb52c67fc844905b
PRASANTH REDDY <prasanthreddy@PRASANTHs-MacBook-Air.local>
2023-06-01 23:08:33 +0530

    version17

---
+++ javaScript/312.js
@@ -0,0 +1,227 @@
+function login312() {
+    let username = document.getElementById("floatingInputTwo").value;
+    let password = document.getElementById("floatingPasswordTwo").value;
+    if (username == "prasanth" && password == "prakarthik") {
+      open("../html/recordings.html");
+    } else if (username == "bindu" && password == "bindu222") {
+      open("../html/recordings.html");
+    } else if (username == "swathi" && password == "swathi123") {
+      open("../html/recordings.html");
+    } else if (username == "sushma" && password == "sushma9988") {
+      open("../html/recordings.html");
+    } else if (username == "pallavi" && password == "pallavi9858") {
+      open("../html/recordings.html");
+    } else if (username == "sameena" && password == "sameena123") {
+      open("../html/recordings.html");
+    } else if (username == "sushmasreee" && password == "sushma123") {
+      open("../html/recordings.html");
+    } else if (username == "balasubramanyam" && password == "balu987") {
+      open("../html/recordings.html");
+    } else if (username == "priyareddy" && password == "priya123") {
+      open("../html/recordings.html");
+    } else if (username == "Bhargavi" && password == "Bhargavi234") {
+      open("../html/recordings.html");
+    } else if (username == "sai sree" && password == "sree123") {
+      open("../html/recordings.html");
+    } else if (username == "sathya sai keerthana" && password == "keerthana345") {
+      open("../html/recordings.html");
+    } else if (username == "saranya" && password == "saranya123") {
+      open("../html/recordings.html");
+    } else if (username == "sai niharika" && password == "harika654") {
+      open("../html/recordings.html");
+    } else if (username == "sai krishna" && password == "krishna555") {
+      open("../html/recordings.html");
+    } else if (username == "sravani" && password == "sravani987") {
+      open("../html/recordings.html");
+    } else if (username == "bhargavreddy" && password == "bhargav123") {
+      open("../html/recordings.html");
+    } else if (username == "gayathri" && password == "gayathri333") {
+      open("../html/recordings.html");
+    } else if (username == "rupasri" && password == "rupa987") {
+      open("../html/recordings.html");
+    } else if (username == "hemalatha" && password == "hemalatha555") {
+      open("../html/recordings.html");
+    } else if (username == "harika" && password == "harika65") {
+      open("../html/recordings.html");
+    } else if (username == "gnana prasuna" && password == "prasuna111") {
+      open("../html/recordings.html");
+    } else if (username == "manasa" && password == "manasa7876") {
+      open("../html/recordings.html");
+    } else if (username == "swathi" && password == "swathi222") {
+      open("../html/recordings.html");
+    } else if (username == "durga prasad" && password == "durga345") {
+      open("../html/recordings.html");
+    } else if (username == "vanicute" && password == "vani111") {
+      open("../html/recordings.html");
+    } else if (username == "mydhili" && password == "mydhili132") {
+      open("../html/recordings.html");
+    } else if (username == "harika" && password == "harika55") {
+      open("../html/recordings.html");
+    } else if (username == "reshma" && password == "reshma999") {
+      open("../html/recordings.html");
+    } else if (username == "mahidhar" && password == "mahidhar234") {
+      open("../html/recordings.html");
+    } else if (username == "divya" && password == "divya345") {
+      open("../html/recordings.html");
+    } else if (username == "meghana" && password == "meghana608") {
+      open("../html/recordings.html");
+    } else if (username == "reshma" && password == "reshu111") {
+      open("../html/recordings.html");
+    } else if (username == "gayathri" && password == "gayathri9988") {
+      open("../html/recordings.html");
+    } else if (username == "seshasai" && password == "sai765") {
+      open("../html/recordings.html");
+    } else if (username == "uday kiran" && password == "uday9988") {
+      open("../html/recordings.html");
+    } else if (username == "srigayathri" && password == "gayathri549") {
+      open("../html/recordings.html");
+    } else if (username == "venkatesh" && password == "venkatesh 222") {
+      open("../html/recordings.html");
+    } else if (username == "mamatha" && password == "mamatha414") {
+      open("../html/recordings.html");
+    } else if (username == "divya sree" && password == "divya65") {
+      open("../html/recordings.html");
+    } else if (username == "vaarshika" && password == "vaarshi55") {
+      open("../html/recordings.html");
+    } else if (username == "krishna vamsi" && password == "vamsi 222") {
+      open("../html/recordings.html");
+    } else if (username == "venkat rohith" && password == "rohith555") {
+      open("../html/recordings.html");
+    } else if (username == "lekhana" && password == "lekhana234") {
+      open("../html/recordings.html");
+    } else if (username == "sandhya" && password == "sandhya987") {
+      open("../html/recordings.html");
+    } else if (username == "aishwarya" && password == "aishwarya123") {
+      open("../html/recordings.html");
+    } else if (username == "sunaina" && password == "sunaina234") {
+      open("../html/recordings.html");
+    } else if (username == "sushma sree" && password == "sushma987") {
+      open("../html/recordings.html");
+    } else if (username == "Bhargavi" && password == "Bhargavi234") {
+      open("../html/recordings.html");
+    } else if (username == "gbhavana" && password == "gbhavana@npm") {
+      open("../html/recordings.html");
+    } else if (username == "babureddy" && password == "reddy@sdr") {
+      open("../html/recordings.html");
+    } else if (username == "reddy" && password == "reddy@mgr") {
+      open("../html/recordings.html");
+    } else if (username == "tanmaieesai" && password == "tanmaiee000") {
+      open("../html/recordings.html");
+    } else if (username == "sruthija" && password == "sruthija987") {
+      open("../html/recordings.html");
+    } else if (username == "suneetha" && password == "suneetha876") {
+      open("../html/recordings.html");
+    } else if (username == "lakshman" && password == "lakshman988") {
+      open("../html/recordings.html");
+    } else if (username == "akshayapriya" && password == "akshaya765") {
+      open("../html/recordings.html");
+    } else if (username == "nirvighna" && password == "nirvighna65") {
+      open("../html/recordings.html");
+    } else if (username == "nehayadav" && password == "neha665") {
+      open("../html/recordings.html");
+    } else if (username == "sravika" && password == "sravika123") {
+      open("../html/recordings.html");
+    } else if (username == "sowmya" && password == "sowmya111") {
+      open("../html/recordings.html");
+    } else if (username == "suchini" && password == "suchini 345") {
+      open("../html/recordings.html");
+    } else if (username == "divyasri" && password == "divya234") {
+      open("../html/recordings.html");
+    } else if (username == "rajasekhar" && password == "rajasekhar123") {
+      open("../html/recordings.html");
+    } else if (username == "Busi Nagendramma" && password == "Nagendramma111") {
+      open("../html/recordings.html");
+    } else if (username == "lasya" && password == "lasya299") {
+      open("../html/recordings.html");
+    } else if (username == "sai likhith" && password == "likhith333") {
+      open("../html/recordings.html");
+    } else if (username == "poojitha" && password == "poojitha23") {
+      open("../html/recordings.html");
+    } else if (username == "karthik reddy" && password == "karthik974") {
+      open("../html/recordings.html");
+    } else if (username == "karthik reddy" && password == "karthik974") {
+      open("../html/recordings.html");
+    } else if (username == "umar abid" && password == "umar03") {
+      open("../html/recordings.html");
+    } else if (username == "keerthana" && password == "keerthana2003") {
+      open("../html/recordings.html");
+    } else if (username == "vamsi krishna" && password == "vamsi1756") {
+      open("../html/recordings.html");
+    } else if (username == "bhargav sai" && password == "sai777") {
+      open("../html/recordings.html");
+    } else if (username == "vamsikrishna" && password == "vamsi777") {
+      open("../html/recordings.html");
+    } else if (username == "vijay" && password == "vijay549") {
+      open("../html/recordings.html");
+    } else if (username == "nikitha" && password == "nikitha555") {
+      open("../html/recordings.html");
+    } else if (username == "ajay kumar" && password == "ajay444") {
+      open("../html/recordings.html");
+    } else if (username == "sumayaa" && password == "sumayaa987") {
+      open("../html/recordings.html");
+    } else if (username == "kjyothi" && password == "kjyothi@qlr") {
+      open("../html/recordings.html");
+    } else if (username == "jahnavi" && password == "jahnavi455") {
+      open("../html/recordings.html");
+    }else if (username == "gowthami" && password == "gowthami455") {
+      open("../html/recordings.html");
+    }else if (username == "dasu" && password == "dasu113") {
+      open("../html/recordings.html");
+    }else if (username == "Mahendra Pavalla" && password == "pavalla65"){
+      open("../html/recordings.html");
+    }else if (username == "prathyusha" && password == "prathyusha123"){
+      open("../html/recordings.html");
+    }else if (username == "narendra" && password == "narendra123"){
+      open("../html/recordings.html");
+    }else if (username == "mahesh" && password == "mahesh234"){
+      open("../html/recordings.html");
+    }else if (username == "poorna" && password == "poorna555"){
+      open("../html/recordings.html");
+    }else if (username == "sai" && password == "sai123"){
+      open("../html/recordings.html");
+    }else if (username == "sagar" && password == "sagar245"){
+      open("../html/recordings.html");
+    }else {
+      alert("YOU ARE NOT A MEMBER INSIDE BTC 😢");
+    }
+  }
+  // -------------function for 313-----------
+  function login313(){
+    let username = document.getElementById("floatingInputThree").value;
+    let password = document.getElementById("floatingPasswordThree").value;
+    if (username == "prasanth" && password == "prakarthik"){
+      open("../html/recordings.html");
+    }else{
+      alert("YOU ARE NOT A MEMBER OF STANDUP313😢")
+    }
+  }
+  // -------------function for 313-----------
+  function login314(){
+    let username = document.getElementById("floatingInputFour").value;
+    let password = document.getElementById("floatingPasswordFour").value;
+    if (username == "prasanth" && password == "prakarthik"){
+      open("../html/recordings.html");
+    }else{
+      alert("YOU ARE NOT A MEMBER OF STANDUP313😢")
+    }
+  }
+  // -------------function for 314-----------
+  function login315(){
+    let username = document.getElementById("floatingInputFour").value;
+    let password = document.getElementById("floatingPasswordFour").value;
+    if (username == "prasanth" && password == "prakarthik"){
+      open("../html/recordings.html");
+    }else{
+      alert("YOU ARE NOT A MEMBER OF STANDUP313😢")
+    }
+  }
+  // -------------function for 315-----------
+  function login316(){
+    let username = document.getElementById("floatingInputFive").value;
+    let password = document.getElementById("floatingPasswordFive").value;
+    if (username == "prasanth" && password == "prakarthik"){
+      open("../html/recordings.html");
+    }else{
+      alert("YOU ARE NOT A MEMBER OF STANDUP313😢")
+    }
+  }
